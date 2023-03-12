3ae340c6be0c1fb36d9a15c3978733bec56d14c9
MateusSartorio <mateussartorio39@gmail.com>
2023-03-11 22:30:45 -0300

    finished, error page added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,18 +7,16 @@ import { Routes, Route } from "react-router-dom";
 import { Layout } from "./components/layout";
 import { Trailer } from "./components/trailer";
 import { Reviews } from "./components/reviews";
+import { NotFound } from "./components/not_found";
 
 export const App = () => {
     const [ movies, setMovies ] = useState([]);
-    const [ movie, setMovie ] = useState();
-    const [ reviews, setReviews ] = useState();
 
     async function getMovies() {
         const response = await fetch("http://192.168.100.35:8080/api/v1/movies");
         const moviesArray = await response.json();
         setMovies(moviesArray);
     }
-
 
     useEffect(() => {
         getMovies();
@@ -32,6 +30,7 @@ export const App = () => {
                     <Route path="/" element={<Home movies={movies}/>}/>
                     <Route path="/trailer/:ytTrailerId" element={<Trailer/>}/>
                     <Route path="/reviews/:movieId" element={<Reviews/>}/>
+                    <Route path="*" element={<NotFound/>}/>
                 </Route> 
             </Routes>
         </div>
---
+++ frontend/src/components/not_found/index.tsx
@@ -0,0 +1,7 @@
+export const NotFound = () => {
+    return (
+        <h1>
+            This page could not be found!
+        </h1>
+    );
+}
